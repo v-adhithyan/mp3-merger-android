79befafef3c0dcdc894efa17a22690610c04392d
v-adhithyan <aavispeaks@gmail.com>
2019-12-27 07:43:26 +0530

    Choose files for merge inside app

---
+++ app/src/main/java/ceg/avtechlabs/avmergemp3/ChooseFileActivity.kt
@@ -0,0 +1,11 @@
+package ceg.avtechlabs.avmergemp3
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ChooseFileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
